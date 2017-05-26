c7d075c9f2a787fdc8903031faa854188f2e0740
aaronryank <rkcoder2002@gmail.com>
2017-05-26 15:12:14 +0000

    Beat my own record

---
+++ c/060-shortest.c
@@ -0,0 +1,3 @@
+/* World's shortest C quine! Credit: Aaron Ryan (me) */
+
+main(s){printf(s="main(s){printf(s=%c%s%1$c,34,s);}",34,s);}
--- c/064-shortest.c
+++
@@ -1,3 +0,0 @@
-/* Credit: Aaron Ryan */ /* Shortest known C quine */
-
-main(s){printf(s="main(s){printf(s=%c%s%c,34,s,34);}",34,s,34);}
