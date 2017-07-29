2f3ba73e57962ce5c6153cb07114990344c4e300
MD XF <rkcoder2002@gmail.com>
2017-07-28 17:55:51 -0700

    Add TIO link

--- c/063-md.c
+++ c/063-md.c
@@ -2,4 +2,5 @@
 
 main(){printf(%c%s%1$c,34,%2$d);}",34,4195728);}
 
-/* note: this won't work on your system. It only works on TIO, link coming soon. If you modify the 4195728 to your system's requirements, it might work. */
+/* note: this won't work on your system. It only works on TIO, link below. If you modify the 4195728 to your system's requirements, it might work. */
+/* https://tio.run/##S9ZNT07@/z83MTNPQ7O6oCgzryRNQwmVq5qsWqxqqJKsY2yio2qkkqJpXasEYpsYWpqaG1kAuf//AwA */
